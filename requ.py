7748b33ed7a26bd4091a460b675ed2fe8e421747
Mark O'Brien <obrnmrk@gmail.com>
2014-12-13 12:08:33 -0500

    initial request
    to find page count. beginning of artist index page scrape.

--- requ.py
+++ requ.py
@@ -1,17 +1,37 @@
 import urllib
 import urllib2
-import lxml.html
+import lxml.html as par
 
-def request():
+def page_count():
 	base_url = 'https://bandcamp.com/artist_index'
 	page_num = 0
-	responses = []
 	init_res = urllib2.urlopen(base_url)
-	html = init_res.read()
+	tree = par.parse(init_res)
+	root = tree.getroot()
+	pages = root.find_class('pagenum')
+	last = pages[len(pages)-1]
+	count_string = last.get('href')
+	garbage, number = count_string.split('=')
+	pagecount = int(number)
+	return pagecount
 
-	return
+def page_scrape(pagina):
+	base_url = 'https://bandcamp.com/artist_index?page='
+	response = urllib2.urlopen(base_url+str(pagina))
+	tree = par.parse(response)
+	root = tree.getroot()
+	elements = root.find_class('bandName')
+	bands = {}
+	for i in range(0,len(elements)-1):
+		print elements[i].keys()
+		print elements[i].items()
+		print elements[i].get('class')
+		bands[elements[i].get('bandName')] = elements[i].get('href')
+	return bands
+
 
 if __name__ == '__main__':
-	stuff = request()
+	bands = page_scrape(0)
+	print bands
 	
 
--- scrape.py
+++ scrape.py
@@ -1,2 +1,6 @@
 import requ.py
 
+count = page_count()
+	responses = []
+	for i in range(0,count):
+		responses.append(page_scrape(i))
