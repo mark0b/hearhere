2be456c893b82ae76c8d8a2078d56e6fbbb7d2b2
Mark O'Brien <obrnmrk@gmail.com>
2014-12-12 19:01:56 -0500

    First lines
    put a little meat on the bones

--- requ.py
+++ requ.py
@@ -2,3 +2,16 @@ import urllib
 import urllib2
 import lxml.html
 
+def request():
+	base_url = 'https://bandcamp.com/artist_index'
+	page_num = 0
+	responses = []
+	init_res = urllib2.urlopen(base_url)
+	html = init_res.read()
+
+	return
+
+if __name__ == '__main__':
+	stuff = request()
+	
+
