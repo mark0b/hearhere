28b0292f6777d17985ab4ebd509fdf36177a969e
chriskuech <kuech@bu.edu>
2014-12-18 13:49:24 -0500

    bug fixes

--- bandcamp.py
+++ bandcamp.py
@@ -28,22 +28,24 @@ def urlopen(url, attempts=3):
 
 
 def scrapeindexes():
-    """crawl the bandcamp artist listings for all the band urls"""
+    """generate band urls by scraping bandcamp artist indexes"""
 
     # get the number of pages
     html = urlopen('https://bandcamp.com/artist_index')
-    if html is None: return None
+    if html is None: return
     e = parse(html).getroot().find_class('pagenum').pop()
     numpages = int(e.text_content())
+    reurl = re.compile(r'http://([^\.]+)\.bandcamp\.com')
 
     # scrape each index page for band urls
-    bands = set()
     for i in range(1, 2 or numpages + 1):
         html = urlopen('https://bandcamp.com/artist_index?page=%d' % i)
         bandlist = parse(html).getroot().get_element_by_id('bandlist')
-        bands |= {a.get('href') for a in bandlist.getiterator('a')}
+        for a in bandlist.getiterator('a'):
+            url = a.get('href')
+            if reurl.match(url):
+                yield url
 
-    return bands
 
 
 
@@ -51,25 +53,24 @@ def scrapeband(url):
     """scrapes a bandcamp band page for the band's info"""
 
     html = urlopen(url)
-    if html is None: return None
+    if html is None: return
     root = parse(html).getroot()
     e = root.get_element_by_id('band-name-location', None)
-    if e is None: return None
+    if e is None: return
 
     return {
         # TODO add back fields
         'location': e.find_class('location secondaryText')[0].text,
         'name': e.find_class('title')[0].text,
-        'tags': [a.get('href') for a in root.find_class('tag')]
+        'tags': [a.text_content() for a in root.find_class('tag')]
     }
 
 
 
 def scrape():
-    urls = scrapeindexes()
-    data = {url: scrapeband(url) for url in urls}
-    return {url: data[url] for url in data if data[url]}
-
+    reid = re.compile(r'http://([^\.]+)\.bandcamp\.com')
+    data = {reid.match(url).group(1): scrapeband(url) for url in scrapeindexes()}
+    return {id0: data[id0] for id0 in data if data[id0]}
 
 
 if __name__ == '__main__':
